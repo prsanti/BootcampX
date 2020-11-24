d00730c70582370b00711914f5ff5106bcfae4ef
Paulo Santiago <p.santiago796@gmail.com>
2020-11-24 21:51:24 +0000

    completed queries iv exercises

---
+++ 4_queries/1_total_teacher_assistance_requests.sql
@@ -0,0 +1,5 @@
+SELECT count(assistance_requests.*) as total_assistance, teachers.name
+  FROM assistance_requests
+  JOIN teachers ON teachers.id = teacher_id
+  WHERE teachers.name = 'Waylon Boehm'
+  GROUP BY teachers.name;
